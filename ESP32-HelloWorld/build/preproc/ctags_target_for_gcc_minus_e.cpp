d80264247c10b559f019916b7274fe663f445ada
Juan Manuel Cáceres <juanmanuel.caceres24@outlook.com>
2023-09-22 14:58:19 -0300

    update

--- ESP32-HelloWorld/build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ ESP32-HelloWorld/build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,31 +1,32 @@
 # 1 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
-const int ledPin = 2;
-hw_timer_t *timer = 
-# 2 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino" 3 4
+
+
+hw_timer_t* timer = 
+# 3 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino" 3 4
                    __null
-# 2 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
+# 3 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
                        ;
 portMUX_TYPE timerMux = {.owner = 0xB33FFFFF,.count = 0} /**< Spinlock initializer */;
 volatile bool timerFlag = false;
 const int timerInterval = 1000;
 
-void __attribute__((section(".iram1" "." "28"))) onTimer() {
+void __attribute__((section(".iram1" "." "28"))) onTimer( ) {
  vPortEnterCritical(&timerMux);
  timerFlag = true;
  vPortExitCritical(&timerMux);
 }
 
-void setup() {
- pinMode(ledPin, 0x03);
- timer = timerBegin(0, 80, true);
- timerAttachInterrupt(timer, &onTimer, true);
- timerAlarmWrite(timer, timerInterval * 1000, true);
- timerAlarmEnable(timer);
+void setup( ) {
+ pinMode( 2, 0x03 );
+ timer = timerBegin( 0, 80, true );
+ timerAttachInterrupt( timer, &onTimer, true );
+ timerAlarmWrite( timer, timerInterval * 1000, true );
+ timerAlarmEnable( timer );
 }
 
-void loop() {
- if (timerFlag) {
-  digitalWrite(ledPin, !digitalRead(ledPin));
+void loop( ) {
+ if ( timerFlag ) {
+  digitalWrite( 2, !digitalRead( 2 ));
   vPortEnterCritical(&timerMux);
   timerFlag = false;
   vPortExitCritical(&timerMux);
--- ESP32-HelloWorld/build/sketch/ESP32-HelloWorld.ino.cpp
+++ ESP32-HelloWorld/build/sketch/ESP32-HelloWorld.ino.cpp
@@ -1,35 +1,37 @@
 #include <Arduino.h>
 #line 1 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
-const int ledPin = 2;
-hw_timer_t *timer = NULL;
+#define LED_PIN 2
+
+hw_timer_t* timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 volatile bool timerFlag = false;
 const int timerInterval = 1000;
 
-#line 13 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
-void setup();
-#line 21 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
-void loop();
-#line 7 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
-void IRAM_ATTR onTimer() {
-	portENTER_CRITICAL_ISR(&timerMux);
+#line 14 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
+void setup( );
+#line 22 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
+void loop( );
+#line 8 "D:\\Documents\\GitHub\\ESP32-Examples\\ESP32-HelloWorld\\ESP32-HelloWorld.ino"
+void IRAM_ATTR onTimer( ) {
+	portENTER_CRITICAL_ISR( &timerMux );
 	timerFlag = true;
-	portEXIT_CRITICAL_ISR(&timerMux);
+	portEXIT_CRITICAL_ISR( &timerMux );
 }
 
-void setup() {
-	pinMode(ledPin, OUTPUT);
-	timer = timerBegin(0, 80, true);
-	timerAttachInterrupt(timer, &onTimer, true);
-	timerAlarmWrite(timer, timerInterval * 1000, true);
-	timerAlarmEnable(timer);
+void setup( ) {
+	pinMode( LED_PIN, OUTPUT );
+	timer = timerBegin( 0, 80, true );
+	timerAttachInterrupt( timer, &onTimer, true );
+	timerAlarmWrite( timer, timerInterval * 1000, true );
+	timerAlarmEnable( timer );
 }
 
-void loop() {
-	if (timerFlag) {
-		digitalWrite(ledPin, !digitalRead(ledPin));
-		portENTER_CRITICAL(&timerMux);
+void loop( ) {
+	if ( timerFlag ) {
+		digitalWrite( LED_PIN, !digitalRead( LED_PIN ));
+		portENTER_CRITICAL( &timerMux );
 		timerFlag = false;
-		portEXIT_CRITICAL(&timerMux);
+		portEXIT_CRITICAL( &timerMux );
   	}
 }
+
