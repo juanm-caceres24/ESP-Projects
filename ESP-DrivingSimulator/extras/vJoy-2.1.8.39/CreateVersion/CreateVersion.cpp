839ff64ba70af960d47b40dc6b78bc54b398ba8e
juanm-caceres24 <juanmanuel.caceres24@outlook.com>
2026-03-08 17:54:54 -0300

    Uploaded vJoy proyect

---
+++ ESP-DrivingSimulator/extras/vJoy-2.1.8.39/CreateVersion/CreateVersion.cpp
@@ -0,0 +1,121 @@
+// CreateVersion.cpp : Defines the entry point for the console application.
+// Based on: https://github.com/mnunberg/git-describe-parse by M. Nunberg
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _WIN32
+#define popen _popen
+#define pclose _pclose
+#endif
+
+static void find_delim(char *s, char **res, int *ix)
+{
+	for (; *ix && s[*ix] != '-'; *ix -= 1);
+	if (*ix) {
+		s[*ix] = '\0';
+		*res = s + *ix + 1;
+	}
+	else {
+		*res = s;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	unsigned int major = 0, mid = 0, minor = 0, commits = 0;
+	char vbuf[4096] = { 0 };
+	char *version="0.0.0", *ncommits="0", *sha="0";
+	FILE *po;
+	int buflen;
+	int outval;
+
+	// Open output file
+	char * filename = argv[1];
+	if (!filename) {
+		fprintf(stderr, "Bad output file name\n");
+		return -5;
+	}
+
+	FILE * f;
+	errno_t err = fopen_s(&f, filename, "w+");
+	if (err || !f)
+	{
+		fprintf(stderr, "Could not oupen output file name (errno=%d)\n", err);
+		return -6;
+	}
+
+	fprintf(stderr, "Starting CreateVersion\n");
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <Outputfile>\n", argv[0]);
+		outval = -1;
+		goto FINAL;
+	}
+
+	/** Get the output */
+	po = popen("git describe --long --match  v*", "r");
+	if (!po) {
+		perror("git describe");
+		outval = -2;
+		goto FINAL;
+	}
+
+	if (!fgets(vbuf, sizeof(vbuf), po)) {
+		fprintf(stderr, "git describe closed stream\n");
+		outval = -3;
+		goto FINAL;
+	}
+	pclose(po);
+	buflen = (int)strlen(vbuf);
+	vbuf[buflen - 2] = '\0';
+	buflen -= 1;
+
+	// SHA1	 (Remove the leading 'g')
+	find_delim(vbuf, &sha, &buflen);
+	sha++;
+
+	// Commit #	 - Will be used as build number
+	find_delim(vbuf, &ncommits, &buflen);
+
+	
+	// Version
+	version = vbuf;
+
+	// Break the version number into its components
+	int nFields = sscanf_s(version, "v%u.%u.%u.%u",&major, &mid, &minor, &commits) ;
+	if (nFields < 3) {
+		fprintf(stderr, "Version string corrupt\n");
+		outval = -4;
+		goto FINAL;
+	}
+
+	// Case of version of type v2.1.8
+	if (nFields == 3)
+		sscanf_s(ncommits, "%u", &commits);
+
+
+
+FINAL:
+	// Put data into file
+	fprintf(f, "#pragma once\n");
+	fprintf(f, "#define VER_H_ %u\n", major);
+	fprintf(f, "#define VER_M_ %u\n", mid);
+	fprintf(f, "#define VER_L_ %u\n", minor);
+	fprintf(f, "#define BUILD %u\n", commits);
+ 	fprintf(f, "\n");
+	fprintf(f, "#define FILEVER\t\t\t\t\"%u, %u, %u, %u\"\n", major, mid, minor, commits);
+	fprintf(f, "#define PRODVER_TAG\t\t\t\"%s\"\n", version);
+	fprintf(f, "#define PRODVER_SHA1\t\t\"%s\"\n", sha);
+ 	fprintf(f, "\n");
+
+
+	return 0;
+}
+
+//#include "stdafx.h"
+//
+//
+//int main()
+//{
+//    return 0;
+//}
+//
---
+++ ESP-DrivingSimulator/extras/vJoy-2.1.8.39/apps/vJoyInstall/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include "targetver.h"
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <basetyps.h>
+#include <cfgmgr32.h>
+#include <Setupapi.h>
+#include <strsafe.h>
+#include <Newdev.h>
+#include <ks.h>
+#include <initguid.h>
+
+
+// TODO: reference additional headers your program requires here
